3e47536703df1f6e191a38dd542a213abe35be28
kajela <titoyut56@gmail.com>
2022-05-10 06:05:48 -0400

    Initial commit

---
+++ src/Family.java
@@ -0,0 +1,8 @@
+public class Family {
+    public static void main(String[] args) {
+        String[] [] names = {{"tito", "mukami", "kajela", "kca"}, {"22","23"} };
+            System.out.println(names[0][0] + names[1][0]);
+            System.out.println(names[0][1] + names[1][1]);
+        }
+    }
+
